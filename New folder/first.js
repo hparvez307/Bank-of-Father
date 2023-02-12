2fe0c4a3e11f0e1ab115be0901db1376cdc9a712
hparvez307 <hparvez307@gmail.com>
2023-02-12 15:38:43 +0600

    second commit

---
+++ New folder/first.js
@@ -0,0 +1,27 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+
+    const passInput = document.getElementById('password');
+    const password = passInput.value;
+
+    if (email === 'hparvez307@gmail.com' && password === 'parvez600697') {
+        window.location.href = 'http://127.0.0.1:5500/bank.html';
+    }
+    else if (email !== 'hparvez307@gmail.com' && password !== 'parvez600697') {
+        alert('Incorrect info');
+    }
+    else if (email !== 'hparvez307@gmail.com') {
+        alert('Incorrect email address');
+    }
+    else if (password !== 'parvez600697') {
+        alert('Incorrect password');
+    }
+})
+
+
+
+
+
+
+
---
+++ New folder/second.js
@@ -0,0 +1,84 @@
+// bank dashboard
+
+document.getElementById('btn-deposit').addEventListener('click', function () {
+
+    // deposit section
+    const inputdeposit = document.getElementById('input-deposit');
+    const deposit = inputdeposit.value;
+    const depositAmount = parseFloat(deposit);
+
+
+    inputdeposit.value = '';
+    if (isNaN(depositAmount) || depositAmount <= 0) {
+
+        return alert('Please input an amount');
+    }
+
+    // total previous deposit
+    const previousTotalDeposit = document.getElementById('deposit-dasbord');
+    const previousDeposit = previousTotalDeposit.innerText;
+    const previousTotalDepositAmount = parseFloat(previousDeposit);
+
+
+    //total deposit update
+    const totalDeposit = previousTotalDepositAmount + depositAmount;
+    previousTotalDeposit.innerText = totalDeposit;
+
+
+    //  balance dashboard
+    const previousBalance = document.getElementById('balance');
+    const totalBalance = previousBalance.innerText;
+    const balance = parseFloat(totalBalance);
+    // update balance dashboard
+    const updateBalance = balance + depositAmount;
+    previousBalance.innerText = updateBalance;
+
+
+
+})
+
+
+// withdraw section
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    // get withdraw amount
+    const withdrawInput = document.getElementById('wi-input');
+    const withdraw = withdrawInput.value;
+    const totalWithdrawAmount = parseFloat(withdraw);
+
+    // validation and reset withdraw input or withdraw amount
+    withdrawInput.value = '';
+    if (isNaN(totalWithdrawAmount)) {
+        return alert('Please enter an amount');
+    }
+
+
+
+    // get and update total withdraw dashboard
+    const previousWithdraw = document.getElementById('previous-withdraw');
+    const previousWithdrawAmount = previousWithdraw.innerText;
+    const totalPreviousWithdraw = parseFloat(previousWithdrawAmount);
+
+
+    // get total previous balance of bap er bank
+
+    const previousBalance = document.getElementById('balance');
+    const previousBalanceAmount = previousBalance.innerText;
+    const totalPreviousBalance = parseFloat(previousBalanceAmount);
+    // alert for withdraw amount
+    if (totalWithdrawAmount > totalPreviousBalance) {
+        return alert('Please enter a valid withdraw amount');
+    }
+
+    // update withdraw dashboard
+
+    const totalWithdraw = totalPreviousWithdraw + totalWithdrawAmount;
+
+    previousWithdraw.innerText = totalWithdraw;
+
+    // update balance dashboard
+
+    const totalBalance = totalPreviousBalance - totalWithdrawAmount;
+    previousBalance.innerText = totalBalance;
+
+
+})
